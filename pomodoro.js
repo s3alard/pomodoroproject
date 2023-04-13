2efe5d683b08f069dd33ea45bb2a6eed33cb2824
s3alard <laszlo.szilard2000@gmail.com>
2023-04-13 14:23:31 +0200

    declare js variables

---
+++ pomodoro.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById("start-button");
+var resetButton = document.getElementById("reset-button");
+var pomodoroMinusButton = document.getElementById("pomodoro-minus-button");
+var pomodoroPlusButton = document.getElementById("pomodoro-plus-button");
+var shortBreakMinusButton = document.getElementById("short-break-minus-button");
+var ShortBreakPlusButton = document.getElementById("short-break-plus-button");
+var longBreakMinusButton = document.getElementById("long-break-minus-button");
+var longBreakPlusButton = document.getElementById("long-break-plus-button");
+
+var mainDisplay = document.getElementById("main-display");
+var pomodoroDisplay = document.getElementById("pomodoro-display");
+var shortBreakDisplay = document.getElementById("short-break-display");
+var longBreakDisplay  = document.getElementById("long-break-display");
